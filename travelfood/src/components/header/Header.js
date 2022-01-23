58cb4e145d10070d0f6790606657659e95630e19
tduncan14 <nbatre1@yahoo.com>
2022-01-22 23:58:43 -0800

    keep moving forward

--- travelfood/src/components/header/Header.js
+++ travelfood/src/components/header/Header.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import{Autocomplete, AutoComplete} from '@react-google-maps/api';
-import {AppBar,ToolBar,Typography,InputBase,Box} from '@material-ui/core';
-import SearchIcon from 'material-ui/icons/Search';
+import{Autocomplete,} from '@react-google-maps/api';
+import {AppBar,Toolbar,Typography,InputBase,Box} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './style'
 
 
 
 const Header = () => {
+
+    const classes = useStyles()
+
 
 
 
 
     return (
         <AppBar position="static">
-            <ToolBar className={classes.toolbar}>
-                 <Typography variant="h5" className={classes.title}>
-                     <Box diplay="flex">
-                      <Typography variant="h6" className={classes.title}>
-                          Explore new places
-                      </Typography>
-                      {/* division */}
-                      <Autocomplete>
-                           <div className={classes.search}>
-                                <div className={classes.searchIcon}>
-                                    <SearchIcon />
-                                </div>
-                                <InputBase placeholder="Search..." classes={{root:classes.inputRoot, input:classes.inputInput}}/>
-                           </div>
-                      </Autocomplete>
-                     </Box>
-                 </Typography>
-            </ToolBar>
-        </AppBar>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h5" className={classes.title}>
+            Travel Advisor
+          </Typography>
+          {/* division */}
+          <Box display="flex">
+            <Typography variant="h6" className={classes.title}>
+              Explore new places
+            </Typography>
+            {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+              </div>
+            {/* </Autocomplete> */}
+          </Box>
+        </Toolbar>
+      </AppBar>
     )
 }
 
--- travelfood/src/components/map/Map.js
+++ travelfood/src/components/map/Map.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper,Typography,useMediaQuery} from '@material-ui/core';
+import Rating from '@material-ui/lab';
 
 
+
+import useStyles from './style';
 
 
 const Map  = () => {
 
-
+  const classes= useStyles()
 
   return (
       <div>
