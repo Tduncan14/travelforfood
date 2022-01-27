1572ea4a62683c39cd95ea568c10d493970ba473
tduncan14 <nbatre1@yahoo.com>
2022-01-26 19:03:09 -0800

    keeep moving forward

--- travelfood/src/components/placedetails/Placedetails.js
+++ travelfood/src/components/placedetails/Placedetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Box,Typography,Button,Card,CardMedia,CardContent,CardActions,Chip, PlaceSharp} from '@material-ui/core';
-import LocationOnOutlined from '@material-ui/icons/LocationOn';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
 import Rating from '@material-ui/lab/Rating';
 
@@ -52,6 +52,29 @@ return(
               {place?.cuisine?.map(({name}) => (
                   <Chip key={name} size="small" label={name} className={classes.chip}/>
               ))}
+
+              {place?.address && (
+                  <Typography gutterBottom variant = "subtitle2" color="textSecondary"  className={classes.subtitle}>
+                      <LocationOnIcon > {place.address} </LocationOnIcon>
+                  </Typography>
+              )}
+
+       {place?.phone && (
+                  <Typography gutterBottom variant = "subtitle2" color="textSecondary"  className={classes.spacing}>
+                      <PhoneIcon/> {place.phone}
+                  </Typography>
+              )}       
+
+              <CardActions>
+                   <Button size="small" color="primary" onClick={()=> window.open(place.web_url,'_blank')}>
+                        Advisor
+                   </Button>
+
+
+                   <Button size="small" color="primary" onClick={()=> window.open(place.website,'_blank')}>
+                        Website
+                   </Button>
+              </CardActions>
           </CardContent>
 
 
